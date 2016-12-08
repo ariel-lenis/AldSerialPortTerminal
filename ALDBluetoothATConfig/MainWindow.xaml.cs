99b175d05bd688cf711cb4c18e2b7db4fe63ba80
Ariel Lenis <ariel.lenisd@gmail.com>
2016-12-08 01:14:32 -0400

    Existing project in vs2010

---
+++ ALDBluetoothATConfig/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using A = ALDSerialPort;
+namespace ALDBluetoothATConfig
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdatePorts();        
+        }
+
+        private void UpdatePorts()
+        {
+            var res = ALDSerialPort.ALDSerialPort.EnumeratePorts();
+            cbCOMPorts.ItemsSource = null;
+            cbCOMPorts.ItemsSource = res;
+            if (res.Count > 0) cbCOMPorts.SelectedIndex = 0;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnBuscar_Click(object sender, RoutedEventArgs e)
+        {
+            UpdatePorts();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Window_SourceInitialized(object sender, EventArgs e)
+        {
+            Type type;
+            /*
+            type= typeof(A.ALDSerialPort.SerialConfiguration.EBitsPerSecond);
+            cbBitsPerSecond.ItemsSource = type.GetEnumValues();
+            cbBitsPerSecond.SelectedItem = ALDSerialPort.ALDSerialPort.SerialConfiguration.EBitsPerSecond.V9600;
+            */
+            cbBitsPerSecond.Items.Add(1200);
+            cbBitsPerSecond.Items.Add(2400);
+            cbBitsPerSecond.Items.Add(4800);
+            cbBitsPerSecond.Items.Add(9600);
+            cbBitsPerSecond.Items.Add(19200);
+            cbBitsPerSecond.Items.Add(38400);
+            cbBitsPerSecond.Items.Add(57600);
+            cbBitsPerSecond.Items.Add(230400);
+            cbBitsPerSecond.SelectedItem = 9600;
+            
+            type = typeof(A.ALDSerialPort.SerialConfiguration.EDataBits);
+            cbDataBits.ItemsSource = type.GetEnumValues();
+            cbDataBits.SelectedItem = ALDSerialPort.ALDSerialPort.SerialConfiguration.EDataBits.V8;
+            
+
+
+
+            type = typeof(System.IO.Ports.StopBits);           
+            cbStopBits.ItemsSource = type.GetEnumValues();
+            cbStopBits.SelectedItem = System.IO.Ports.StopBits.One;
+
+            type = typeof(System.IO.Ports.Parity);
+            cbParity.ItemsSource = type.GetEnumValues();
+            cbParity.SelectedItem = System.IO.Ports.Parity.None; 
+        }
+
+        private void btnStartConfiguration_Click(object sender, RoutedEventArgs e)
+        {
+            A.ALDSerialPort.PreviousInformation prev = cbCOMPorts.SelectedItem as A.ALDSerialPort.PreviousInformation;
+            A.ALDSerialPort.SerialConfiguration sconf = new A.ALDSerialPort.SerialConfiguration();
+            A.ALDSerialPort.TotalConfiguration configuration = new A.ALDSerialPort.TotalConfiguration(prev.Port, sconf);
+            configuration.Configuration.ConfigBitsPerSecond = (int)cbBitsPerSecond.SelectedItem;
+            configuration.Configuration.ConfigDataBits = (A.ALDSerialPort.SerialConfiguration.EDataBits)this.cbDataBits.SelectedItem;
+            configuration.Configuration.ConfigParity = (System.IO.Ports.Parity)this.cbParity.SelectedItem;
+            configuration.Configuration.ConfigStopBits = (System.IO.Ports.StopBits)this.cbStopBits.SelectedItem;
+
+            A.ALDSerialPort serial = new A.ALDSerialPort(configuration);
+            try
+            {
+                serial.OpenPort();
+                WPFComandosAT w = new WPFComandosAT(serial);
+                w.ShowDialog();
+            }
+            catch
+            {
+                MessageBox.Show("No se puede abrir este puerto.", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void cbBitsPerSecond_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
---
+++ ALDBluetoothATConfig/WPFComandosAT.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ALDBluetoothATConfig
+{
+	/// <summary>
+	/// Interaction logic for WPFTestearComunicacion.xaml
+	/// </summary>
+	public partial class WPFComandosAT : Window
+	{
+        Stopwatch sw;
+        Timer timer;
+        string snew;
+        string sold;
+        public enum HC06Baudrate
+        {
+            v1200bps = 1,
+            v2400bps,
+            v4800bps,
+            v9600bps,
+            v19200bps,
+            v38400bps,
+            v57600bps,
+            v115200bps,
+            v230400bps
+        }
+        private ALDSerialPort.ALDSerialPort serial;
+
+		public WPFComandosAT()
+		{
+			this.InitializeComponent();
+
+            var type = typeof(HC06Baudrate);
+            cbBaudrate.ItemsSource = type.GetEnumValues();
+            cbBaudrate.SelectedItem = HC06Baudrate.v9600bps;
+            sw = new Stopwatch();
+            sw.Start();
+            snew = sold = "";
+            timer = new Timer(1000);
+            timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+
+            cbParity.Items.Add("None Ninguna");
+            cbParity.Items.Add("Odd Impar");
+            cbParity.Items.Add("Even Par");
+            cbParity.SelectedIndex = 0;
+		}
+
+        void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            this.Dispatcher.Invoke(new Action(delegate {
+                this.IsEnabled = true;
+            }));
+            timer.Stop();
+        }
+
+        public WPFComandosAT(ALDSerialPort.ALDSerialPort serial):this()
+        {            
+            this.serial = serial;
+            serial.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(serial_DataReceived);
+            serial.ErrorReceived += new System.IO.Ports.SerialErrorReceivedEventHandler(serial_ErrorReceived);
+        }
+
+        void serial_ErrorReceived(object sender, System.IO.Ports.SerialErrorReceivedEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        void serial_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string res = serial.Serial.ReadExisting();
+            if (sw.ElapsedMilliseconds > 1000)
+            {
+                if(snew!="") sold = snew + "\n" + sold;
+                snew = "[" + DateTime.Now.TimeOfDay.ToString() + "] " + res;
+                sw.Restart();
+            }
+            else
+                snew += res;
+            this.Dispatcher.Invoke(new Action(delegate
+            {
+                if (snew != "")
+                    txtResults.Text = snew + "\n" + sold;
+                else
+                    txtResults.Text = sold;
+            }));
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            serial.ClosePort();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnTryComunication_Click(object sender, RoutedEventArgs e)
+        {
+            this.IsEnabled = false;
+            timer.Start();
+            serial.SendData("AT");
+        }
+
+        private void btnGetFirmwareVersion_Click(object sender, RoutedEventArgs e)
+        {
+            this.IsEnabled = false;
+            timer.Start();
+
+            serial.SendData("AT+VERSION");
+            
+        }
+
+        private void btnBaudrateChange_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Estas cambiando el modo de comunicacion por lo tanto deberias volver a abrir el programa con la nueva configuracion para que todo este OK.");
+            this.IsEnabled = false;
+            timer.Start();
+
+            
+
+            HC06Baudrate res = (HC06Baudrate)cbBaudrate.SelectedItem;
+            serial.SendData("AT+BAUD"+(int)res);
+
+        }
+
+        private void btnNameChange_Click(object sender, RoutedEventArgs e)
+        {
+            this.IsEnabled = false;
+            timer.Start();
+
+            serial.SendData("AT+NAME" + txtName.Text);
+        }
+
+        private void btnPINChange_Click(object sender, RoutedEventArgs e)
+        {
+            int val;
+            if (!int.TryParse(txtPIN.Text, out val))
+            {
+                MessageBox.Show("El pin tiene que ser de 4 numeros");
+                return;
+            }
+            
+            this.IsEnabled = false;
+            timer.Start();
+
+            serial.SendData("AT+PIN" + txtPIN.Text);
+        }
+
+        private void btnParity_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Estas cambiando el modo de comunicacion por lo tanto deberias volver a abrir el programa con la nueva configuracion para que todo este OK.");
+            this.IsEnabled = false;
+            timer.Start();
+
+            serial.SendData("AT+P" + cbParity.Text[0]);
+        }
+	}
+}
